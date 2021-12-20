51adf67aabc5390ed5a1bab84f11123d00d2ea5e
Altobert <alberto.sanmartin@usach.cl>
2021-12-20 14:07:07 -0300

    enlanzando funciones

---
+++ principal.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "funciones.h"
+
+int main(int argc, char *argv[]){
+
+	//int n = 0, flag = 0;
+    printf("Saludos Cordiales -m\n");
+	//recibirArgumentos(argc, argv, &n, &flag);
+	/*if(flag==1){
+		printf("Se utilizo flag -m\n");
+	}*/
+	//printf("El argumento de flag -h es: %d\n", n);
+	//printGlobal();
+}
